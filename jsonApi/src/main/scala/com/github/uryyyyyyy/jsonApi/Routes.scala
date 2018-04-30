033c92f49d58704f2dd300d64445c6126ae4c0b3
uryyyyyyy <koki305@gmail.com>
2018-04-30 23:35:58 +0900

    validation

--- jsonApi/src/main/scala/com/github/uryyyyyyy/jsonApi/Routes.scala
+++ jsonApi/src/main/scala/com/github/uryyyyyyy/jsonApi/Routes.scala
@@ -1,9 +1,10 @@
-package com.github.uryyyyyyy.jsonApi.route
+package com.github.uryyyyyyy.jsonApi
 
 import akka.http.scaladsl.server._
 import com.github.uryyyyyyy.jsonApi.controller.HelloController
 import com.github.uryyyyyyy.jsonApi.dto.JsonFormatCustom._
 import com.github.uryyyyyyy.jsonApi.dto._
+import com.github.uryyyyyyy.jsonApi.route.{ErrorHandlerDirective, ValidationDirectives}
 import com.google.inject.Injector
 
 class Routes(injector: Injector) extends ValidationDirectives with ErrorHandlerDirective {
